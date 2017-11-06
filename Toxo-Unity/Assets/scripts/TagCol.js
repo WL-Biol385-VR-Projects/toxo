8de4d1bbc64ec1be80265f74dd0fab60fed42fab
murphyp18 <murphyp18@mail.wlu.edu>
2017-11-06 17:45:52 -0500

    Got rid of lights, made IRG and ROP18 have lights and start the process of interaction

---
+++ Toxo-Unity/Assets/scripts/TagCol.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+var thing1 : GameObject;
+var thing2 : GameObject;
+var objectTag : String;
+
+function OnTriggerEnter (other : Collider) {
+        if(other.gameObject.tag == objectTag){
+            thing1.SetActive(true);
+            thing2.SetActive(true);
+    }
+}
---
+++ Toxo-Unity/Assets/scripts/Tag_on_off.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+var thingon : GameObject;
+var thingoff : GameObject;
+var objectTag : String;
+
+function OnTriggerEnter (other : Collider) {
+        if(other.gameObject.tag == objectTag){
+            thingon.SetActive(true);
+            thingoff.SetActive(false);
+    }
+}
