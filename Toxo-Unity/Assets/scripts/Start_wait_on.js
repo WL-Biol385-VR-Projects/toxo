f93df1d3431c8205fc79d5fd07cf55a5361f2a73
murphyp18 <murphyp18@mail.wlu.edu>
2017-12-02 15:55:57 -0500

    Sound tutorial with paul

---
+++ Toxo-Unity/Assets/scripts/Start_wait_on.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+var thingon : GameObject;
+var duration : float = 0;
+
+function Start () {
+    yield WaitForSeconds(duration);
+    thingon.SetActive(true); 
+}
---
+++ Toxo-Unity/Assets/scripts/VoiceOn.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class VoiceOn : MonoBehaviour
+{
+    //place this script onto the GameObject that will hold the audio source
+
+    private string[] m_Keywords;
+
+    private KeywordRecognizer m_Recognizer;
+    public String Name;
+    public GameObject thingon;
+    public GameObject thingoff;
+
+    void Start()
+    {
+        m_Keywords = new string[1];
+        m_Keywords[0] = Name;
+        m_Recognizer = new KeywordRecognizer(m_Keywords);
+        m_Recognizer.OnPhraseRecognized += OnPhraseRecognized;
+        m_Recognizer.Start();
+    }
+
+    private void OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        if (args.text == m_Keywords[0])
+        {
+            thingon.SetActive(true);
+            thingoff.SetActive(false);
+        }
+    }
+}
